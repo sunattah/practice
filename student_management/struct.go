b54394eafa21740e885e97e8ee952c8c26ff34fc
sunattah <sundayattah319@gmail.com>
2026-06-08 16:25:27 +0100

    my work

---
+++ student_management/struct.go
@@ -0,0 +1,20 @@
+package main
+
+type student struct {
+	name  string
+	age   int
+	Grade string
+}
+
+func disStuInf(input string) {
+
+}
+
+func main() {
+	stud := student{
+		name:  "adam",
+		age:   32,
+		Grade: "A",
+	}
+
+}
